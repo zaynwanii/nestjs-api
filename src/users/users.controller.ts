41cb97808f4dae092720f07b848f5d6e68b080f4
ZAID BIN ZAHOOR <zaidwaani@gmail.com>
2024-11-19 11:15:24 +0530

    Initial Commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+    @Get()
+    findAll(){
+        return []
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id:string){
+        return {id}
+    }
+
+    @Post()
+   
+    create(@Body() user:{}){
+        return user
+    }
+
+    @Patch(':id')
+    update(@Param('id') id:string, @Body() userUpdate:{}){
+        return {id,...userUpdate};
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:string){
+        return { id }
+    }
+}
